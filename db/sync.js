9c46a8016a0e9d18223dcb1eae875def95ce2969
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-10-27 22:14:22 -0400

    added data, and changed models some

--- db/sync.js
+++ db/sync.js
@@ -1,13 +1,14 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const { DB_NAME } = process.env;
+const {DB_NAME} = process.env;
 
 (async () => {
   try {
     const statement = `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`;
     const db = await mysql.createConnection("mysql://root@127.0.0.1:3306");
     await db.execute(statement);
+    console.log(`${DB_NAME} created`);
     process.exit();
   } catch (err) {
     console.log(err);
--- models/index.js
+++ models/index.js
@@ -4,14 +4,17 @@ const Comment = require("./Comment");
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: 'CASCADE',
 });
 
 Post.hasMany(Comment, {
   foreignKey: "post_id",
+  onDelete: 'CASCADE',
 });
 
-Comment.belongsTo(Post, {
+Comment.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: 'CASCADE',
 });
 
 module.exports = { User, Post, Comment };
